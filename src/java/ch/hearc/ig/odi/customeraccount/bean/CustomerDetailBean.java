4d55b8f013123ffb59ead8e10863a69347081d36
JulmyS <julmyst@gmail.com>
2015-11-29 10:28:53 +0100

    Affichage des détails du client avec la mise à jour de la liste des client (homepage)

---
+++ src/java/ch/hearc/ig/odi/customeraccount/bean/CustomerDetailBean.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hearc.ig.odi.customeraccount.bean;
+
+import ch.hearc.ig.odi.customeraccount.business.Customer;
+import ch.hearc.ig.odi.customeraccount.service.Services;
+import javax.ejb.Stateful;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author JulmyS
+ */
+
+@Stateful
+@ManagedBean(name = "customerDetail")
+@SessionScoped
+
+public class CustomerDetailBean {
+
+    @Inject
+    Services services;
+    
+    private Customer customer;
+    
+    public String saveDetailsOfCustomer(int number){
+        this.customer = this.services.getCustomer(number);
+        return "detailClient";
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customers) {
+        this.customer = customers;
+    }
+    
+}
